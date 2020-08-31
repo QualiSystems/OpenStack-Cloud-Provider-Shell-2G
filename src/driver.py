843cf5f81a4dabc743aa4a94f99b91753313759f
Kyrylo Maksymenko <saklar13@gmail.com>
2020-08-31 14:16:05 +0300

    add commands

--- src/driver.py
+++ src/driver.py
@@ -1,224 +1,160 @@
+from typing import List
+
 from cloudshell.shell.core.resource_driver_interface import ResourceDriverInterface
-from cloudshell.shell.core.driver_context import InitCommandContext, AutoLoadCommandContext, ResourceCommandContext, \
-    AutoLoadDetails, CancellationContext, ResourceRemoteCommandContext
+from cloudshell.shell.core.driver_context import (
+    InitCommandContext,
+    AutoLoadCommandContext,
+    ResourceCommandContext,
+    AutoLoadDetails,
+    CancellationContext,
+    ResourceRemoteCommandContext,
+)
 from cloudshell.shell.core.session.cloudshell_session import CloudShellSessionContext
 from cloudshell.shell.core.session.logging_session import LoggingSessionContext
 
-from cloudshell.cp.openstack.openstack_shell import OpenStackShell
+from cloudshell.cp.core.cancellation_manager import CancellationContextManager
+from cloudshell.cp.core.request_actions import (
+    DeployVMRequestActions,
+    DeployedVMActions,
+    GetVMDetailsRequestActions,
+)
+from cloudshell.cp.openstack import constants
+from cloudshell.cp.openstack.flows import delete_instance
+from cloudshell.cp.openstack.flows import DeployAppFromNovaImgFlow
+from cloudshell.cp.openstack.flows import PowerFlow
+from cloudshell.cp.openstack.flows import GetVMDetailsFlow
+from cloudshell.cp.openstack.flows import refresh_ip
+from cloudshell.cp.openstack.models import OSNovaImgDeployedApp
+from cloudshell.cp.openstack.models import OSNovaImgDeployApp
+from cloudshell.cp.openstack.os_api.api import OSApi
+from cloudshell.cp.openstack.os_api.services import validate_conf_and_connection
 from cloudshell.cp.openstack.resource_config import OSResourceConfig
 
 
 class OpenstackShell2GDriver(ResourceDriverInterface):
-    SHELL_NAME = "Openstack Shell 2G"
+    SHELL_NAME = constants.SHELL_NAME
 
     def __init__(self):
-        self.os_shell = OpenStackShell()
+        pass
 
     def initialize(self, context: InitCommandContext):
-        """Called every time a new instance of the driver is created."""
         pass
 
     def get_inventory(self, context: AutoLoadCommandContext) -> AutoLoadDetails:
-        """Called when the cloud provider resource is created in the inventory.
-
-        Method validates the values of the cloud provider attributes, entered by the user
-        as part of the cloud provider resource creation
-        """
+        """Check connection to OpenStack."""
         with LoggingSessionContext(context) as logger:
-            logger.info("Starting Autoload command...")
+            logger.info("Starting Autoload command")
             api = CloudShellSessionContext(context).get_api()
             conf = OSResourceConfig.from_context(self.SHELL_NAME, context, api)
-            return self.os_shell.get_inventory(conf, logger)
+            os_api = OSApi(conf, logger)
+            validate_conf_and_connection(os_api, conf)
+            return AutoLoadDetails([], [])
 
-    def Deploy(self, context, request, cancellation_context=None):
-        """
-        Called when reserving a sandbox during setup, a call for each app in the sandbox.
+    def Deploy(
+        self,
+        context: ResourceCommandContext,
+        request: str,
+        cancellation_context: CancellationContext,
+    ) -> str:
+        """Deploy image."""
+        with LoggingSessionContext(context) as logger:
+            logger.info("Starting Deploy command")
+            logger.debug(f"Request: {request}")
+            api = CloudShellSessionContext(context).get_api()
+            conf = OSResourceConfig.from_context(self.SHELL_NAME, context, api)
+            cancellation_manager = CancellationContextManager(cancellation_context)
+            DeployVMRequestActions.register_deployment_path(OSNovaImgDeployApp)
+            request_actions = DeployVMRequestActions.from_request(request, api)
+            os_api = OSApi(conf, logger)
+            return DeployAppFromNovaImgFlow(
+                conf, cancellation_manager, os_api, logger
+            ).deploy(request_actions)
 
-        Method creates the compute resource in the cloud provider - VM instance or container.
+    def PowerOn(self, context: ResourceRemoteCommandContext, ports: List[str]):
+        """Method spins up the VM."""
+        with LoggingSessionContext(context) as logger:
+            logger.info("Starting PowerOn command")
+            api = CloudShellSessionContext(context).get_api()
+            conf = OSResourceConfig.from_context(self.SHELL_NAME, context, api)
+            DeployedVMActions.register_deployment_path(OSNovaImgDeployedApp)
+            resource = context.remote_endpoints[0]
+            actions = DeployedVMActions.from_remote_resource(resource, api)
+            os_api = OSApi(conf, logger)
+            return PowerFlow(os_api, actions.deployed_app, logger).power_on()
 
-        If App deployment fails, return a "success false" action result.
+    def remote_refresh_ip(
+        self,
+        context: ResourceRemoteCommandContext,
+        ports: List[str],
+        cancellation_context: CancellationContext,
+    ):
+        """Retrieves the VM's updated IP address from the OpenStack."""
+        with LoggingSessionContext(context) as logger:
+            logger.info("Starting Remote Refresh IP command")
+            api = CloudShellSessionContext(context).get_api()
+            conf = OSResourceConfig.from_context(self.SHELL_NAME, context, api)
+            DeployedVMActions.register_deployment_path(OSNovaImgDeployedApp)
+            resource = context.remote_endpoints[0]
+            actions = DeployedVMActions.from_remote_resource(resource, api)
+            os_api = OSApi(conf, logger)
+            refresh_ip(os_api, actions.deployed_app, conf)
 
-        :param ResourceCommandContext context:
-        :param str request: A JSON string with the list of requested deployment actions
-        :param CancellationContext cancellation_context:
-        :return:
-        :rtype: str
-        """
-
-        '''
-        # parse the json strings into action objects
-        actions = self.request_parser.convert_driver_request_to_actions(request)
-        
-        # extract DeployApp action
-        deploy_action = single(actions, lambda x: isinstance(x, DeployApp))
-        
-        # if we have multiple supported deployment options use the 'deploymentPath' property 
-        # to decide which deployment option to use. 
-        deployment_name = deploy_action.actionParams.deployment.deploymentPath
-                
-        deploy_result = _my_deploy_method(context, actions, cancellation_context)
-        return DriverResponse(deploy_result).to_driver_response_json()
-        '''
-        pass
-
-    def PowerOn(self, context, ports):
-        """
-        Called when reserving a sandbox during setup, a call for each app in the sandbox can also be run manually by the sandbox end-user from the deployed App's commands pane
-
-        Method spins up the VM
-
-        If the operation fails, method should raise an exception.
-
-        :param ResourceRemoteCommandContext context:
-        :param ports:
-        """
-        pass
-
-    def remote_refresh_ip(self, context, ports, cancellation_context):
-        """
-
-        Called when reserving a sandbox during setup, a call for each app in the sandbox can also be run manually by the sandbox end-user from the deployed App's commands pane
-
-        Method retrieves the VM's updated IP address from the cloud provider and sets it on the deployed App resource
-        Both private and public IPs are retrieved, as appropriate.
-
-        If the operation fails, method should raise an exception.
-
-        :param ResourceRemoteCommandContext context:
-        :param ports:
-        :param CancellationContext cancellation_context:
-        :return:
-        """
-        pass
-
-    def GetVmDetails(self, context, requests, cancellation_context):
-        """
-        Called when reserving a sandbox during setup, a call for each app in the sandbox can also be run manually by the sandbox
-        end-user from the deployed App's VM Details pane
-
-        Method queries cloud provider for instance operating system, specifications and networking information and
-        returns that as a json serialized driver response containing a list of VmDetailsData.
-
-        If the operation fails, method should raise an exception.
-
-        :param ResourceCommandContext context:
-        :param str requests:
-        :param CancellationContext cancellation_context:
-        :return:
-        """
-        pass
-
-    # </editor-fold>
+    def GetVmDetails(
+        self,
+        context: ResourceCommandContext,
+        requests: str,
+        cancellation_context: CancellationContext,
+    ) -> str:
+        """Get instance operating system, specifications and networking information."""
+        with LoggingSessionContext(context) as logger:
+            logger.info("Starting Deploy command")
+            logger.debug(f"Requests: {requests}")
+            api = CloudShellSessionContext(context).get_api()
+            conf = OSResourceConfig.from_context(self.SHELL_NAME, context, api)
+            cancellation_manager = CancellationContextManager(cancellation_context)
+            GetVMDetailsRequestActions.register_deployment_path(OSNovaImgDeployedApp)
+            actions = GetVMDetailsRequestActions.from_request(requests, api)
+            os_api = OSApi(conf, logger)
+            return GetVMDetailsFlow(
+                conf, cancellation_manager, os_api, logger
+            ).get_vm_details(actions)
 
     def PowerCycle(self, context, ports, delay):
-        """ please leave it as is """
         pass
 
-    def PowerOff(self, context, ports):
-        """
-        Called during sandbox's teardown can also be run manually by the sandbox end-user from the deployed App's commands pane
+    def PowerOff(self, context: ResourceRemoteCommandContext, ports: List[str]):
+        """Method shuts down (or powers off) the VM instance."""
+        with LoggingSessionContext(context) as logger:
+            logger.info("Starting PowerOn command")
+            api = CloudShellSessionContext(context).get_api()
+            conf = OSResourceConfig.from_context(self.SHELL_NAME, context, api)
+            DeployedVMActions.register_deployment_path(OSNovaImgDeployedApp)
+            resource = context.remote_endpoints[0]
+            actions = DeployedVMActions.from_remote_resource(resource, api)
+            os_api = OSApi(conf, logger)
+            return PowerFlow(os_api, actions.deployed_app, logger).power_off()
 
-        Method shuts down (or powers off) the VM instance.
+    def DeleteInstance(self, context: ResourceRemoteCommandContext, ports: List[str]):
+        """Deletes the VM from the OpenStack."""
+        with LoggingSessionContext(context) as logger:
+            logger.info("Starting PowerOn command")
+            api = CloudShellSessionContext(context).get_api()
+            conf = OSResourceConfig.from_context(self.SHELL_NAME, context, api)
+            DeployedVMActions.register_deployment_path(OSNovaImgDeployedApp)
+            resource = context.remote_endpoints[0]
+            actions = DeployedVMActions.from_remote_resource(resource, api)
+            os_api = OSApi(conf, logger)
+            delete_instance(os_api, actions.deployed_app)
 
-        If the operation fails, method should raise an exception.
-
-        :param ResourceRemoteCommandContext context:
-        :param ports:
-        """
-        pass
-
-    def DeleteInstance(self, context, ports):
-        """
-        Called during sandbox's teardown or when removing a deployed App from the sandbox
-
-        Method deletes the VM from the cloud provider.
-
-        If the operation fails, method should raise an exception.
-
-        :param ResourceRemoteCommandContext context:
-        :param ports:
-        """
-        pass
-
-    def ApplyConnectivityChanges(self, context, request):
-        """
-        Called during the orchestration setup and also called in a live sandbox when
-        and instance is connected or disconnected from a VLAN
-        service or from another instance (P2P connection).
-
-        Method connects/disconnect VMs to VLANs based on requested actions (SetVlan, RemoveVlan)
-        It's recommended to follow the "get or create" pattern when implementing this method.
-
-        If operation fails, return a "success false" action result.
-
-        :param ResourceCommandContext context: The context object for the command with resource and reservation info
-        :param str request: A JSON string with the list of requested connectivity changes
-        :return: a json object with the list of connectivity changes which were carried out by the driver
-        :rtype: str
-        """
-        pass
-
-    # todo remove the command
-    def PrepareSandboxInfra(self, context, request, cancellation_context):
-        """
-        Called in the beginning of the orchestration flow (preparation stage), even before Deploy is called.
-
-        Prepares all of the required infrastructure needed for a sandbox operating with L3 connectivity.
-        For example, creating networking infrastructure like VPC, subnets or routing tables in AWS, storage entities such as S3 buckets, or
-        keyPair objects for authentication.
-        In general, any other entities needed on the sandbox level should be created here.
-
-        Note:
-        PrepareSandboxInfra can be called multiple times in a sandbox.
-        Setup can be called multiple times in the sandbox, and every time setup is called, the PrepareSandboxInfra method will be called again.
-        Implementation should support this use case and take under consideration that the cloud resource might already exist.
-        It's recommended to follow the "get or create" pattern when implementing this method.
-
-        When an error is raised or method returns action result with success false
-        Cloudshell will fail sandbox creation, so bear that in mind when doing so.
-
-        :param ResourceCommandContext context:
-        :param str request:
-        :param CancellationContext cancellation_context:
-        :return:
-        :rtype: str
-        """
-        '''
-        # parse the json strings into action objects
-        actions = self.request_parser.convert_driver_request_to_actions(request)
-        
-        action_results = _my_prepare_connectivity(context, actions, cancellation_context)
-        
-        return DriverResponse(action_results).to_driver_response_json()    
-        '''
-        pass
-
-    # todo remove the command
-    def CleanupSandboxInfra(self, context, request):
-        """
-        Called at the end of reservation teardown
-
-        Cleans all entities (beside VMs) created for sandbox, usually entities created in the
-        PrepareSandboxInfra command.
-
-        Basically all created entities for the sandbox will be deleted by calling the methods: DeleteInstance, CleanupSandboxInfra
-
-        If a failure occurs, return a "success false" action result.
-
-        :param ResourceCommandContext context:
-        :param str request:
-        :return:
-        :rtype: str
-        """
-        '''
-        # parse the json strings into action objects
-        actions = self.request_parser.convert_driver_request_to_actions(request)
-        
-        action_results = _my_cleanup_connectivity(context, actions)
-
-        return DriverResponse(action_results).to_driver_response_json()    
-        '''
-        pass
+    def ApplyConnectivityChanges(
+        self, context: ResourceCommandContext, request: str
+    ) -> str:
+        """Connects/disconnect VMs to VLANs based on requested actions."""
+        with LoggingSessionContext(context) as logger:
+            logger.info("Starting PowerOn command")
+            api = CloudShellSessionContext(context).get_api()
+            conf = OSResourceConfig.from_context(self.SHELL_NAME, context, api)
+            os_api = OSApi(conf, logger)
 
     def SetAppSecurityGroups(self, context, request):
         """
@@ -241,8 +177,4 @@ class OpenstackShell2GDriver(ResourceDriverInterface):
         pass
 
     def cleanup(self):
-        """
-        Destroy the driver session, this function is called every time a driver instance is destroyed
-        This is a good place to close any open sessions, finish writing to log files, etc.
-        """
         pass
